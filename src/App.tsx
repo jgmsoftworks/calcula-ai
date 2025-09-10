0e083990dd41987377378698a5f24172dc1d5534
jotavec <jotave.face@gmail.com>
2025-09-10 09:58:33 -0300

    style: apply dark theme and submenu styling

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider
       attribute="class"
-      defaultTheme="light"
+      defaultTheme="dark"
       enableSystem={false}
       disableTransitionOnChange={false}
     >
